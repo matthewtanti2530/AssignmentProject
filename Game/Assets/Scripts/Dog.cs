bcc08b8d95905f6369c19329c4b4495edc1d177c
SkittishFeline <74646676+SkittishFeline@users.noreply.github.com>
2020-12-26 10:40:02 +0100

    Created Dog
    - Inserted a Cube (representing the Dog)
    - Added the Dog's script

---
+++ Game/Assets/Scripts/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    //This variable contains the speed of the Dog
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
