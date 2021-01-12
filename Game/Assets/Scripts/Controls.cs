b71296ecab9612be523334c41b8531a95bd7b33d
SkittishFeline <74646676+SkittishFeline@users.noreply.github.com>
2021-01-12 09:13:49 +0100

    Added a ControlsMenu + buttons
    Added a Controls Menu and some buttons (Controls button for the MainMenu, Back button for the ControlsMenu)
    
    Buttons (still need coding)

---
+++ Game/Assets/Scripts/Controls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controls : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        SceneManager.LoadScene("Controls");
+    }
+
+    void Update()
+    {
+        
+    }
+}
