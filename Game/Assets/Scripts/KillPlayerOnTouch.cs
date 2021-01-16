fec8dee91b051c06869d41aeacb9dfcf1003affb
matthewtanti2530 <75424511+matthewtanti2530@users.noreply.github.com>
2021-01-16 22:29:51 +0100

    added Jump,Timer,Objects
    I added trees, jump, timer for 12 sc if he dosn't move foward, can go back to menu when you die and also logs to jump the water.

---
+++ Game/Assets/Scripts/KillPlayerOnTouch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayerOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.GetComponent<Dog>() !=null)
+        {
+            Destroy(collision.gameObject);
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
